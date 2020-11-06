66c3dbaae5758ff50995cfd2127bb6f2eb654bf7
Eduardo Corzo <eduardo.corzo@sarsy-srl.com>
2020-11-06 20:08:58 -0300

    Se agregan assets y se crea pagina de home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React, {Fragment} from "react";
+import "../assets/css/home.css";
+/**
+ * @return {string} app view.
+ * @param {Object} props
+ */
+export function Home(props) {
+  return (
+    <Fragment>
+      <div className="flex-container">
+        <div>
+          <h2>W O L O X</h2>
+          <h2>Bienvenido a tu entrevista tecnica</h2>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </Fragment>
+  );
+}

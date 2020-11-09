add146579d9d86ac3f03cee0aa018b861468710b
Eduardo Corzo <eduardo.corzo@sarsy-srl.com>
2020-11-08 23:32:44 -0300

    Fix busqueda por nombre

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,28 +1,29 @@
-import React, {Fragment, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import {useForm} from "react-hook-form";
 
 export function SearchBar(props) {
-    const [searchTerm, setSearchTerm] = useState();
+    const [searchTerm, setSearchTerm] = useState("");
     const {handleSubmit} = useForm();
 
     const editSearchTerm = (e) =>{
         setSearchTerm(e.target.value);
     };
 
-    const dynamicSearch = () => {
-        console.log(searchTerm);
+    useEffect(() => {
+        console.log("AA");
+        console.log(props.list);
         if (searchTerm != "") {
             return props.setResults(props.list.filter((item) => item.tech.toLowerCase().includes(searchTerm.toLowerCase())));
         } else {
             return props.setResults(props.list);
         }
-    };
+    }, [searchTerm]);
 
     return (
         <Fragment>
             <form onSubmit={handleSubmit(() => dynamicSearch())}>
                 <input type="text" onChange={(e) => {
-                    editSearchTerm(e); dynamicSearch();
+                    editSearchTerm(e);
                 }}/>
                 <input type="submit" />
             </form>

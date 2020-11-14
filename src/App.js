c24640653835775e1ea1285baf61a102fa7b5128
Eduardo Corzo <eduardo.corzo@sarsy-srl.com>
2020-11-14 19:22:14 -0300

    Se agrega redirect necesarios con routing.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 import * as Pages from "./pages";
-import {Route, BrowserRouter as Router, Redirect, Switch} from 'react-router-dom'; // eslint-disable-line
+import {Route, BrowserRouter as Router, Redirect, Switch, BrowserRouter} from 'react-router-dom'; // eslint-disable-line
+import {PrivateRoute} from "./helpers/PrivateRoute";
 
 /**
  * App function.
@@ -9,16 +10,17 @@ import {Route, BrowserRouter as Router, Redirect, Switch} from 'react-router-dom
  */
 function App() {
     return (
-        <div className="App">
+        <BrowserRouter>
             <Router>
                 <Switch>
                     <Route exact path="/Home" component={Pages.Home}/>
                     <Route exact path="/Login" component={Pages.Login}/>
                     <Route exact path="/Register" component={Pages.Register} />
-                    <Route exact path="/Technologies" component={Pages.Technologies}/>
+                    <PrivateRoute exact path="/Technologies" component={Pages.Technologies}/>
+                    <Redirect from="/" to="/Home" />
                 </Switch>
             </Router>
-        </div>
+        </BrowserRouter>
     );
 }
 
